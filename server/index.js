6dad9365d85cf9ef3bbed2ff1d56500393ad864f
‪Vladimir Lihatchov‬‏ <lichvladimir@gmail.com>
2024-07-23 00:07:13 +0300

    server initalizing commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.set('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
+    res.set('Content-Type', 'application/json');
+    next();
+});
